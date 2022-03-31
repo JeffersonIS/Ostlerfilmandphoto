8bcf45a3a59aec53b690d956446707807ee76a6f
Jefferson Ostler <jefferson.ostler@gmail.com>
2022-03-31 16:33:06 -0600

    added new video data

--- src/data/videoGalleryData.js
+++ src/data/videoGalleryData.js
@@ -11,9 +11,21 @@ const videoGalleryData = [
         src: "https://www.youtube.com/embed/OuZgTSi2550",
     },
     {
-        name: "Felix & Melany",
+        name: "Jantzen & Lindsey",
         sessionID: 1,
-        imgPath: 'https://ik.imagekit.io/ostlerfilmandphoto/VideoThumbnails/MelanyThumbnail_4yhvXtLNhgk.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646180677841',
+        imgPath: 'https://ik.imagekit.io/ostlerfilmandphoto/VideoThumbnails/Jantzen-Lindsey-Still_1.25.1_46aazaeAz.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1648764598436',
+        src: "https://www.youtube.com/embed/IJd6ptuxmx4",
+    },
+    {
+        name: "Jonny & Kimber",
+        sessionID: 2,
+        imgPath: 'https://ik.imagekit.io/ostlerfilmandphoto/VideoThumbnails/Jonny-Kimber-Still_1.8.1_177DEdPPpL.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1648764372073',
+        src: "https://www.youtube.com/embed/92SOaEms5qM",
+    },
+    {
+        name: "Felix & Melany",
+        sessionID: 3,
+        imgPath: 'https://ik.imagekit.io/ostlerfilmandphoto/VideoThumbnails/still_1.61.1_QNnGvUw9y.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1648764759394',
         src: "https://www.youtube.com/embed/yw8w3FUa9Sw",
     }
 ];
